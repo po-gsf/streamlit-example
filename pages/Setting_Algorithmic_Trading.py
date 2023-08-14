9f9b77e734a90ab2fb27d6a573f96c39029fe164
sjshin <sj_belief@gsf.dooray.com>
2023-08-14 11:24:26 +0900

    add

--- pages/Setting_Algorithmic_Trading.py
+++ pages/Setting_Algorithmic_Trading.py
@@ -3,4 +3,25 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
-st.caption('Setting_Algorithmic_Trading')
+
+
+##############################################################
+# Main func ~
+##############################################################
+
+def initMainUI() :
+    st.header('ALGORITHMIC TRADING SETTINGS')
+    initRiskSlider()
+    
+
+    return
+
+
+def initRiskSlider() :
+    st.subheader('Risk Percent')
+    st.slider('', min_value=0, max_value=100)
+    st.caption('The closer to 100, the higher the risk.')
+    return
+
+initMainUI()
+
--- pages/Setting_User.py
+++ pages/Setting_User.py
@@ -3,5 +3,73 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
+import requests
 
-st.caption('Setting_User')
+##############################################################
+# Setting func ~
+##############################################################
+baseHeaders = {"accept":"application/json"}
+server_url = 'http://127.0.0.1:8000'
+
+def postKey(accessKey: str, secretKey: str):
+    response = requests.post(server_url +'/postPrivatekey', 
+                      json={'accessKey':accessKey,
+                            'secretKey':secretKey})
+    print(response.text)
+    return
+
+##############################################################
+# Main func ~
+##############################################################
+class privateKey:
+    accessKey = ''
+    secretKey = ''
+
+def initMainUI() :
+    st.header('USER SETTINGS')
+
+    initAccessKey()
+    initSecretKey()
+    
+    st.button('SETTING', on_click=setPrivateKeyInServer())
+    return
+
+
+def initAccessKey() :
+    st.subheader('accessKey')
+    AccesskeyType = st.radio(
+        "What\'s your accessKey Type?",
+        ('File', 'Text'))
+    
+    if AccesskeyType == 'File':
+        FileUploader_AccessKey = st.file_uploader('accessKey_file')
+        if FileUploader_AccessKey is not None:
+            privateKey.accessKey = FileUploader_AccessKey.read()
+    if AccesskeyType == 'Text':
+        TextInput_AccessKey = st.text_input('accessKey_text') 
+        privateKey.accessKey = TextInput_AccessKey
+    return
+
+def initSecretKey() :
+    st.subheader('secretKey')
+    SecretkeyType = st.radio(
+        "What\'s your secretKey Type?",
+        ('File', 'Text'))
+
+    if SecretkeyType == 'File':
+        FileUploader_SecretKey = st.file_uploader('secretKey_file') 
+        if FileUploader_SecretKey is not None:
+            privateKey.secretKey = FileUploader_SecretKey.read()
+    if SecretkeyType == 'Text':
+        TextInput_SecretKey = st.text_input('secretKey_text') 
+        privateKey.secretKey = TextInput_SecretKey
+    return
+
+def setPrivateKeyInServer() :
+    print('=========================')
+    print(privateKey.accessKey)
+    print(privateKey.secretKey)
+    postKey(privateKey.accessKey, privateKey.secretKey)
+    return
+
+initMainUI()
